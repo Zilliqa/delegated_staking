12a2c6018b8bac923029eb510df5842f1a48a076
DrZoltanFazekas <zoltanfazekas@yahoo.com>
2024-11-25 23:37:20 +0100

    Fix unstaking everything

--- script/unstake_Delegation.s.sol
+++ script/unstake_Delegation.s.sol
@@ -49,6 +49,7 @@ contract Unstake is Script {
             );
 
             if (amount == 0) {
+                vm.prank(msg.sender);
                 amount = INonLiquidDelegation(address(delegation)).getDelegatedStake();
             }
         } else
